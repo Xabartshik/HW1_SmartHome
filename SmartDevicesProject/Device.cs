53769b95faccc21c1b06d0b773c282c5a84597e4
Xabartshik <danoshlakov12@gmail.com>
2025-06-24 15:03:09 +1000

    Другой вариант сеттера Name

--- SmartDevicesProject/Device.cs
+++ SmartDevicesProject/Device.cs
@@ -11,13 +11,10 @@ namespace SmartDevicesProject
         public int Id { get; set; }
         private string _name;
         public string? Name { get { return _name; } 
-            set { 
-                
-                _name = value ?? $"Device-{Id}";
-                _name = _name.Trim();
-                if (String.IsNullOrEmpty(_name)) {
-                    throw new ArgumentException("Строка не может быть пустой");
-                }
+            set {
+                value ??= $"Device-{Id}";
+                //На случай отправки "   " в качестве имени устройства
+                _name = value.Trim() ?? $"Device-{Id}";
             }
         }
         protected SmartHub? currentHub;
